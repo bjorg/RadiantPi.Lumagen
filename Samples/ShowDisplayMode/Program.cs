fa1004f53dd6e7385abcfa9f87edcd56ab9c58e5
Steve G. Bjorg <steve.bjorg@gmail.com>
2021-10-27 17:55:00 -0700

    simplify sample

--- Samples/ShowDisplayMode/Program.cs
+++ Samples/ShowDisplayMode/Program.cs
@@ -9,20 +9,14 @@ using var client = new RadianceProClient(new RadianceProClientConfig {
 });
 
 // hook-up event handler
-client.DisplayModeChanged += ShowDisplayMode;
-
-// wait until the enter key is pressed
-Console.WriteLine("Listening for events. Press ENTER to exit.");
-Console.ReadLine();
-
-// remove event handler
-client.DisplayModeChanged -= ShowDisplayMode;
-
-// function acting on events
-void ShowDisplayMode(object? sender, DisplayModeChangedEventArgs args) {
+client.DisplayModeChanged += delegate (object? sender, DisplayModeChangedEventArgs args) {
     Console.WriteLine("=== DISPLAY MODE ===");
     Console.WriteLine();
     Console.WriteLine(JsonSerializer.Serialize(args.DisplayMode, new JsonSerializerOptions {
         WriteIndented = true
     }));
-}
+};
+
+// wait until the enter key is pressed
+Console.WriteLine("Listening for events. Press ENTER to exit.");
+Console.ReadLine();
